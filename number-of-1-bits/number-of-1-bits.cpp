162a20cf4fede6b13054e278a47c3dcba6adbb4a
Bishal Pandit <55312712+bishalpandit@users.noreply.github.com>
2021-11-14 21:25:12 +0530

    Time: 4 ms (41.69%), Space: 5.9 MB (46.49%) - LeetHub

---
+++ number-of-1-bits/number-of-1-bits.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        int cnt = 0;
+        
+        for(int i=0; i<32; i++) {
+            
+            if(1&n)
+                cnt++;
+            n >>= 1;
+        }
+        
+        return cnt;
+    }
+};
