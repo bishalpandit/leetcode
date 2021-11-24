2f181942e246953ac661e05ee0c363f50ea4f7ad
Bishal Pandit <55312712+bishalpandit@users.noreply.github.com>
2021-11-24 21:54:25 +0530

    Time: 33 ms (27.29%), Space: 19.3 MB (56.04%) - LeetHub

---
+++ move-zeroes/move-zeroes.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        
+        
+       int i=0,k=0;
+        
+       for(int i=0;i<nums.size();i++) {
+           
+           if(nums[i]!=0)
+                nums[k++] = nums[i];
+       }
+        
+        while(k<nums.size()) {
+            
+            nums[k++] = 0;
+        }
+        
+    }
+};
