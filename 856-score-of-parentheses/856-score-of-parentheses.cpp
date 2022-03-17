90890ae563625bf8c02de877c0480a94d69ac341
Bishal Pandit <55312712+bishalpandit@users.noreply.github.com>
2022-03-17 14:06:58 +0530

    Time: 5 ms (14.59%), Space: 6.2 MB (8.12%) - LeetHub

--- 856-score-of-parentheses/856-score-of-parentheses.cpp
+++ 856-score-of-parentheses/856-score-of-parentheses.cpp
@@ -1,60 +1,24 @@
 class Solution {
-    pair<int,int> solve(int idx, string& s, int len) {
-        if(s[idx] == '(' and s[idx+1] == ')') {
-            return {1,idx+1};
-        }
-        
-        int curScore = 0;
-    
-        int cur = idx + 1;
-        
-        for(; cur < len; cur++) {
-            if(s[cur] == '(') {
-                pair<int,int> p = solve(cur, s, len);
-                curScore += p.first;
-                cur = p.second;
-            }
-            
-            else 
-                break;
-        }
-        
-    
-        return {2*curScore,cur};
-        
-    }
+
 public:
     int scoreOfParentheses(string s) {
         int len = s.length();
-        int i = 0;
-        int ans = 0;
-        vector<pair<int,int>> streaks;
+        stack<int> st;
+        st.push(0); // Push 0 for the answer score.
         
-        for(int i=0; i<len; i++) {
-            stack<char> st;
-            int start = i;
-            
-            do {
-                if(s[i] == '(')
-                    st.push(s[i]);
-                else
-                    st.pop();
-                i++;
-            }while(!st.empty());
-            
-            i--;
-            
-            streaks.push_back({start, i});
+        for(int i = 0; i < len; ++i) {
+            if(s[i] == '(') 
+                st.push(0);
+            else {
+                int cur = st.top(); st.pop();
+                int prev = st.top(); st.pop();
+                
+                st.push(prev + max(2*cur, 1));
+            }
         }
         
-        
-        
-        for(auto p: streaks) {
-            cout << p.first << " " << p.second << endl;
-            ans += solve(p.first, s, p.second).first;
-        }
-        
-        return ans;
+        return st.top();
     }
-    
 };
+// if ( -> 0, ) -> 1 if it's just (), 2*0 then take 1. But if ((())), then mult by 2.here max(2*0,1) 1 -> max(2*1,1) 2 -> max(2*2,1)
+    
