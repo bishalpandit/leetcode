8f07da9da020437b30bbb61bee2465e8a9643aa5
Bishal Pandit <55312712+bishalpandit@users.noreply.github.com>
2022-06-08 21:46:06 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (78.83%) - LeetHub

--- 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.cpp
+++ 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.cpp
@@ -1,18 +1,10 @@
 class Solution {
-    bool is_palindrome(string &s) {
-        int l = 0, r = s.size() - 1;
-        
-        while(l <= r) {
-            if(s[l] != s[r])
-                return false;
-            l++; r--;
-        }
-        
-        return true;
-    }
+
 public:
     int removePalindromeSub(string s) {
-        return is_palindrome(s) ? 1 : 2;
+        string t = s;
+        reverse(t.begin(), t.end());
+        return t == s ? 1 : 2;
     }
 };
 
