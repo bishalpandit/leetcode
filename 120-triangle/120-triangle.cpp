2df82f9c0de42b03890930af9f65dd224c7d1343
Bishal Pandit <55312712+bishalpandit@users.noreply.github.com>
2022-06-13 23:37:15 +0530

    Time: 12 ms (28.50%), Space: 8.9 MB (49.02%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -1,16 +1,16 @@
 class Solution {
-    int dp[201][201];
-    int f(vector<vector<int>> &t, int r, int c, int n) {
-        if(r == n - 1) return t[r][c]; // last row huh? only option, just take dude
-        
-        if(dp[r][c] != -1) return dp[r][c]; // Have I seen you before? Yes if I'm not -1.
-        
-        return dp[r][c] = t[r][c] + min(f(t, r + 1, c, n), f(t, r + 1, c + 1, n)); // go left, go right, get me minimum value.
-    }
+
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
-        memset(dp, -1, sizeof(dp));
-        return f(triangle, 0, 0, n);
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+        
+        dp[n - 1] = triangle[n - 1];
+        
+        for(int i = n - 2; i >= 0; i--)
+            for(int j = 0; j <= i; j++) 
+                dp[i][j] = triangle[i][j] + min(dp[i + 1][j], dp[i + 1][j + 1]);
+        
+        return dp[0][0];
     }
 };
