299d5c68a4bbd44ad7190bf12df294a4d140b477
Bishal Pandit <55312712+bishalpandit@users.noreply.github.com>
2022-06-19 11:37:56 +0530

    Time: 4 ms (43.99%), Space: 5.9 MB (62.01%) - LeetHub

--- 202-happy-number/202-happy-number.cpp
+++ 202-happy-number/202-happy-number.cpp
@@ -1,30 +1,23 @@
 class Solution {
-    int sumOfSquareDigits(int n) {
+    int next(int n) {
         int sum = 0;
-        
         while(n) {
-            sum += (n%10) * (n%10);
+            sum += (n%10) * (n%10); //
             n /= 10;
         }
-        
         return sum;
     }
 public:
     bool isHappy(int n) {
-        unordered_set<int> s;
+        int fast, slow;
+        fast = next(n);
+        slow = n;
         
-        s.insert(n);
-        
-        while(n != 1) {
-            n = sumOfSquareDigits(n);
-            if(s.count(n))
-                return false;
-            s.insert(n);
+        while(fast != 1 and slow != fast) {
+            slow = next(slow);
+            fast = next(next(fast));
         }
         
-        return true;
+        return fast == 1;
     }
 };
-
-// 7 3 = 49 + 9 58 25 + 64 89 = 64 + 81 = 145 = 1 + 16 + 25 = 50 = 25 = 4 + 25 = 29
-// 29 = 4 + 81 = 85 = 64 + 25 
