2da504fea114664441cd2c3cbb12530a9370eeda
Bishal Pandit <55312712+bishalpandit@users.noreply.github.com>
2022-06-20 19:58:34 +0530

    Time: 104 ms (58.42%), Space: 74 MB (19.31%) - LeetHub

--- 820-short-encoding-of-words/820-short-encoding-of-words.cpp
+++ 820-short-encoding-of-words/820-short-encoding-of-words.cpp
@@ -1,17 +1,67 @@
 class Solution {
+    
+    struct Node {
+    Node* links[26];
+    bool flag;
+    bool containsKey(char c) {
+        return links[c - 'a'] != NULL;
+    }
+    void insertKey(char c, Node* node) {
+        links[c - 'a'] = node;
+    }
+    Node* next(char c) {
+        return links[c - 'a'];
+    }
+};
+
+class Trie {
+    Node* root;
+    public: 
+
+    Trie() {
+        root = new Node();
+    }
+
+    void insert(string word) {
+        Node* node = root;
+
+        for(auto &c: word) {
+            if(!node->containsKey(c))
+                node->insertKey(c, new Node());
+            node = node->next(c);
+        }
+
+        node->flag = true; // A strings ends here.
+    }
+    
+    bool startsWith(string prefix) {
+        Node* node = root;
+
+        for(auto &c: prefix) {
+            if(!node->containsKey(c))
+                return false;
+            node = node->next(c);
+        }
+        return true;
+    }
+};
 public:
     int minimumLengthEncoding(vector<string>& words) {
-        int n = words.size();
-        unordered_set<string> good(words.begin(), words.end());
-        
-        for(auto &w: words)
-            for(int i = 1; i < w.size(); i++) 
-                good.erase(w.substr(i));
-        
+
+        Trie *t = new Trie();
         int ans = 0;
         
-        for(auto &w: good)
-            ans += w.size() + 1;
+        for(auto &w: words)
+            reverse(w.begin(), w.end());
+        
+        sort(words.begin(), words.end());
+        reverse(words.begin(), words.end());
+        
+        for(auto &w: words) 
+            if(!t->startsWith(w)) {
+                t->insert(w);
+                ans += w.size() + 1;
+            }
         
         return ans;
     }
