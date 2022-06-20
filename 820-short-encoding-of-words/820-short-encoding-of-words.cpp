ce685f2994ef6bc15bc532df163bd6f3ea69153b
Bishal Pandit <55312712+bishalpandit@users.noreply.github.com>
2022-06-20 19:24:31 +0530

    Time: 90 ms (62.87%), Space: 17.3 MB (81.19%) - LeetHub

--- 820-short-encoding-of-words/820-short-encoding-of-words.cpp
+++ 820-short-encoding-of-words/820-short-encoding-of-words.cpp
@@ -2,36 +2,21 @@ class Solution {
 public:
     int minimumLengthEncoding(vector<string>& words) {
         int n = words.size();
-        int ans = 0;
+        unordered_set<string> good(words.begin(), words.end());
         
         for(auto &w: words)
-            reverse(w.begin(), w.end());
+            for(int i = 1; i < w.size(); i++) 
+                good.erase(w.substr(i));
         
-        sort(words.begin(), words.end());
+        int ans = 0;
         
-        
-        for(int i = 0; i < n - 1; i++) {
-            int j = 0;
-            
-            while(j < words[i].size()) {
-                if(words[i][j] != words[i + 1][j]) {
-                    ans += words[i].size() + 1;
-                    break;
-                }
-                j++;
-            }
-        }
-        
-        ans += words[n - 1].size() + 1;
-        
-        cout << ans;
+        for(auto &w: good)
+            ans += w.size() + 1;
         
         return ans;
     }
 };
 
 /*
-[ em emit emita]
 
-moneygram#kick
 */
