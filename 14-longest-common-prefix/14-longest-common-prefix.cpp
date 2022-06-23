31ccf87f747b3e9b0f0e967576d553e686f3d1f5
Bishal Pandit <55312712+bishalpandit@users.noreply.github.com>
2022-06-23 11:16:17 +0530

    Time: 18 ms (6.85%), Space: 12.3 MB (8.39%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.cpp
+++ 14-longest-common-prefix/14-longest-common-prefix.cpp
@@ -1,29 +1,32 @@
 class Solution {
+    string f(vector<string>& strs, int lo, int hi) {
+        if(lo == hi) return strs[lo];
+        int x = (lo + hi)/2;
+        
+        string left = f(strs, lo, x); 
+        string right = f(strs, x + 1, hi);
+        return commonPrefix(left, right);
+    }
+    
+    string commonPrefix(string left, string right) {
+        string temp;
+        
+        for(int i = 0; i < left.size() and i < right.size(); i++) {
+            if(left[i] != right[i])
+                return temp;
+            temp += left[i];
+        }
+        
+        return temp;
+    }
 public:
     string longestCommonPrefix(vector<string>& strs) {
         int n = strs.size();
-        if(n == 1)
-            return strs[0];
-        
-        string ans;
-        int len = 200;
-        
-        for(int i = 0; i < n - 1; i++) {
-            string s1 = strs[i], s2 = strs[i + 1];
-            string temp;
-            
-            for(int j = 0; j < s1.size() and j < s2.size(); j++) {
-                if(s1[j] != s2[j])
-                    break;
-                temp += s1[j];
-            }
-            
-            if(temp.size() < len) {
-                len = temp.size();
-                ans = temp;
-            }
-        }
-
-        return ans;
+        return f(strs, 0, n - 1);
     }
 };
+
+/*
+fl, flow, flock,  flower, fluent
+
+*/
