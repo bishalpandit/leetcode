73ba7d4363686c61c2aec3a19cd201922b1f9dcb
Bishal Pandit <55312712+bishalpandit@users.noreply.github.com>
2022-06-28 09:46:10 +0530

    Time: 143 ms (27.45%), Space: 17.4 MB (58.78%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -3,29 +3,28 @@ class Solution {
 public:
     int minDeletions(string s) {
         unordered_map<char, int> m;
-        
         for(auto &c: s)
             m[c]++;
         
         vector<int> freq;
-        
         for(auto &[f, s]: m)
             freq.push_back(s);
         
         sort(begin(freq), end(freq), greater<int>());
-        
-        stack<int> st;
+
         int ans = 0;
         
-        st.push(freq[0]);
-        
         for(int i = 1; i < freq.size(); i++) {
-            if(st.top() <= freq[i]) {
-                ans += (st.top() == 0) ? freq[i] : (freq[i] - st.top()) + 1;
-                st.push(max(st.top() - 1, 0));
+            if(freq[i - 1] == 0) { // When last char while traversing has 0 freq.
+                ans += freq[i]; 
+                freq[i] = 0;
+                continue;
             }
-            else
-                st.push(freq[i]);
+            
+            if(freq[i - 1] <= freq[i]) {
+                ans += max(0, freq[i] - freq[i - 1]) + 1; // Characters need to be removed
+                freq[i] = min(freq[i - 1] - 1, freq[i]); // New freq count
+            }
         }
         
         return ans;
@@ -34,11 +33,6 @@ public:
 
 /*
 
-aaabbcce  
-
-3 2 1 0 
-
-aaaaa eeeee bbb ccc ddd ff k i j
-0 3
-1 + 1 + 2 + 2
+5 4 3 2 1 2
+0 2
 */
