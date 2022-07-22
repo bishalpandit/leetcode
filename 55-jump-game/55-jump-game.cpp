11c3da450ce55e14e5890035a0ff83e5eb0b396f
Bishal Pandit <55312712+bishalpandit@users.noreply.github.com>
2022-07-22 10:04:43 +0530

    Time: 1093 ms (9.87%), Space: 48.7 MB (19.35%) - LeetHub

---
+++ 55-jump-game/55-jump-game.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n = nums.size();
+        vector<bool> dp(n, false);
+        
+        dp[n - 1] = true;
+        
+        for(int i = n - 2; i >= 0; i--) {
+            for(int j = i; j < n and j <= i + nums[i]; j++) {
+                if(dp[j]) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        
+        return dp[0];
+    }
+};
